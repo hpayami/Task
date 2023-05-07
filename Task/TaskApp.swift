69468fbe591afe761aedd03a5b288589cbb3df37
Hossein Payami <hpayami@hotmail.com>
2023-05-07 12:17:06 +0330

    Initial Commit

---
+++ Task/TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp.swift
+//  Task
+//
+//  Created by PhD Hossein Payami on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
